abb1df3ad712014927446b9778607b93d221d58f
toomaas <34375423+toomaas@users.noreply.github.com>
2022-12-26 01:15:46 +0000

    add trending book list

--- src/screens/Home/HomeScreen.styles.ts
+++ src/screens/Home/HomeScreen.styles.ts
@@ -1,11 +1,7 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {alignItems: 'center'},
 });
 
 export default styles;
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,29 +1,45 @@
 import {useTheme} from '@react-navigation/native';
-import React, {useEffect} from 'react';
-import {Button, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, Button, FlatList, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import BooksApi from '../../api/openlibrary/books';
+import {Book} from '../../api/openlibrary/books/types';
+import BookCover from '../../components/BookCover';
 import styles from './HomeScreen.styles';
 import {HomeScreenProps} from './HomeScreen.types';
 
 const HomeScreen: React.FunctionComponent<HomeScreenProps> = props => {
   const {navigation} = props;
   const {colors} = useTheme();
-  useEffect(() => {}, []);
+
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await BooksApi().trending();
+        setBooks(result.books);
+      } catch (error) {
+        Alert.alert('Error fetching books', JSON.stringify(error));
+      }
+    })();
+  }, []);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={{color: colors.text}}>Home Screen</Text>
-      <Button
-        title="Get trending"
-        onPress={() => {
-          BooksApi()
-            .trending()
-            .then(data => {
-              console.log('in view data', data);
-            })
-            .catch(error => {
-              console.log('in view error', error);
-            });
+      <FlatList
+        horizontal
+        data={books}
+        renderItem={book => {
+          return (
+            <View style={{margin: 5, alignItems: 'center'}}>
+              <BookCover
+                uri={`https://covers.openlibrary.org/b/id/${book.item.coverImageId}-S.jpg`}
+              />
+              <Text style={{color: colors.text}}>{book.item.title}</Text>
+            </View>
+          );
         }}
       />
       <Button
@@ -32,7 +48,7 @@ const HomeScreen: React.FunctionComponent<HomeScreenProps> = props => {
           navigation.navigate('BookScreen');
         }}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
