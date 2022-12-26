4f2704fba1094d6ebd2560c1cda62d82ddf3f9cf
toomaas <34375423+toomaas@users.noreply.github.com>
2022-12-26 01:27:41 +0000

    add book touchable navigation

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -1,6 +1,7 @@
-import {useTheme} from '@react-navigation/native';
+import {useNavigation, useTheme} from '@react-navigation/native';
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {HomeScreenPropsNavigationProp} from '../../screens/Home/HomeScreen.types';
 import BookCover from '../BookCover';
 import {BookProps} from './Book.types';
 
@@ -8,15 +9,22 @@ const Book: React.FunctionComponent<BookProps> = props => {
   const {book} = props;
   const {colors} = useTheme();
 
+  const navigation = useNavigation<HomeScreenPropsNavigationProp>();
+
   useEffect(() => {}, []);
 
   return (
-    <View style={{margin: 5, alignItems: 'center'}}>
-      <BookCover
-        uri={`https://covers.openlibrary.org/b/id/${book.coverImageId}-S.jpg`}
-      />
-      <Text style={{color: colors.text}}>{book.title}</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('BookScreen');
+      }}>
+      <View style={{margin: 5, alignItems: 'center'}}>
+        <BookCover
+          uri={`https://covers.openlibrary.org/b/id/${book.coverImageId}-S.jpg`}
+        />
+        <Text style={{color: colors.text}}>{book.title}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
