f6c546bcd05ad6680860524325e760ea1b144337
toomaas <34375423+toomaas@users.noreply.github.com>
2022-12-28 01:41:06 +0000

    add optional style prop to bookCover

--- src/components/BookCover/BookCover.types.ts
+++ src/components/BookCover/BookCover.types.ts
@@ -1,6 +1,9 @@
+import {ImageStyle, StyleProp} from 'react-native';
+
 export type BookCoverProps = {
   coverId: number;
   size: 'S' | 'M' | 'L';
+  style?: StyleProp<ImageStyle>;
 };
 
 export type ImageDimensions = {
