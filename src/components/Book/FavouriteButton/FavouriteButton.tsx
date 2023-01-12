f61c780ac22b328e06e6a526050e6ea8ea4c3788
toomaas <34375423+toomaas@users.noreply.github.com>
2023-01-12 00:28:00 +0000

    save favourites in redux
    add redux toolkit

--- src/components/Book/FavouriteButton/FavouriteButton.tsx
+++ src/components/Book/FavouriteButton/FavouriteButton.tsx
@@ -1,16 +1,30 @@
 import {useTheme} from '@react-navigation/native';
 import {FAB} from '@rneui/base';
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {addFavourite, removeFavourite} from '../../../redux/slices/bookSlice';
+import {RootState} from '../../../redux/store';
 import {FavouriteButtonProps} from './FavouriteButton.types';
 
 const FavouriteButton: React.FunctionComponent<
   FavouriteButtonProps
 > = props => {
+  const {
+    work: {key},
+  } = props;
   const {colors} = useTheme();
+  const favourites = useSelector((state: RootState) => state.book.favourites);
+  const dispatch = useDispatch();
+
+  console.log(favourites);
+
+  const isFavourite = favourites.includes(key);
 
   return (
     <FAB
-      buttonStyle={{backgroundColor: colors.primary}}
+      buttonStyle={{
+        backgroundColor: isFavourite ? colors.notification : colors.primary,
+      }}
       icon={{
         name: 'star-plus-outline',
         type: 'material-community',
@@ -18,6 +32,11 @@ const FavouriteButton: React.FunctionComponent<
       }}
       placement="right"
       size="large"
+      onPress={() => {
+        isFavourite
+          ? dispatch(removeFavourite(key))
+          : dispatch(addFavourite(key));
+      }}
     />
   );
 };
